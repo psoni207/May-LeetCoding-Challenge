259432eeb95d713ac5b22bfff8ae477e4c39d9e2
psoni207 <psoni207@gmail.com>
2020-05-04 21:38:45 +0530

    Day-3

---
+++ 04. NumberComplement.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int res = 0;
+        int msb = findMsb(num);
+        
+		//Suppose num = 9 (1001) then msb bit is 4
+		// create a mask with first 4 bits as 1. res = 1111
+        for(int i=0; i<msb; i++){
+            res += (1<<i);
+        }
+        
+		//just xor res with num, you'll get the complement.. 
+		//1111 ^ 1001 = 0110
+        res = res  ^ num;
+        return res;
+    }
+    
+    int findMsb(int num){
+        int res = 0;
+        while(num){
+            res += 1;
+            num/=2;
+        }
+        
+        return res;
+    }
+};
