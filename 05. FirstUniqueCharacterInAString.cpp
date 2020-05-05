c9f36aaa3ec9d3179b8f89e2e7919848b776b91a
psoni207 <psoni207@gmail.com>
2020-05-05 22:15:14 +0530

    Day-5

---
+++ 05. FirstUniqueCharacterInAString.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int freq[26] = {0};
+        int n = s.size();
+        
+        for(int i = 0; i < n; i++){
+            freq[s[i]-'a'] += 1;
+        }
+        
+        for(int i = 0; i < n; i++){
+            if(freq[s[i]-'a'] == 1){
+                return i;
+            }
+        }
+        
+        return -1;
+        
+    }
+};
