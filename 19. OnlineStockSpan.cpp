eea9dff090bbaea70a614bf20339ad8f3933fe9b
psoni207 <psoni207@gmail.com>
2020-08-01 21:29:03 +0530

    Day-19

--- 19. OnlineStockSpan.cpp
+++ 19. OnlineStockSpan.cpp
@@ -38,3 +38,40 @@ public:
  * StockSpanner* obj = new StockSpanner();
  * int param_1 = obj->next(price);
  */
+
+class StockSpanner {
+    private:
+        stack<pair<int, int> > st;
+        
+public:
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        
+        // 100 80 60 70 60 75 85
+        // 100 1
+        // 100 1 80 1
+        // 100 1 80 1 60 1
+        // 100 1 80 1 70 2
+        // 100 1 80 1 70 2 60 1
+        // 100 1 80 1 75 4
+        // 100 1 85 6
+        int counter = 1;
+        while(!st.empty() && st.top().first <= price){
+            counter += st.top().second;
+            st.pop();
+        }
+        st.push({price,counter}); 
+        
+        return counter;
+        
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
